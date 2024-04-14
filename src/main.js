08521e65a13b949e8d48fef46ee40bfa7759d17f
Tokyo772 <merokyry33@gmail.com>
2024-04-15 00:03:11 +0200

    z

---
+++ src/main.js
@@ -0,0 +1,41 @@
+// const elements = {
+//   content: document.querySelector('.js-content'),
+//   text: document.querySelector('.js-text'),
+// };
+
+// let count = 10;
+// elements.text.textContent = `Залишилось ${count} sec`;
+
+// const id = setInterval(() => {
+//   count -= 1;
+//   elements.text.textContent = `Залишилось ${count} sec`;
+//   if (count === 0) {
+//     clearInterval(id);
+//     elements.text.style.display = 'none';
+//   }
+// }, 1000);
+
+// setTimeout(() => {
+//   clearInterval(id);
+//   elements.text.style.display = 'none';
+// }, count * 1000);
+//TODO:
+// setTimeout(() => {
+//   console.log('hello');
+// }, 0);
+// console.time('for');
+
+// for (let i = 0; i < 10000; i++) {
+//   console.log(i);
+// }
+// for (let i = 0; i < 10000; i++) {
+//   console.log(i);
+// }
+// console.timeEnd('for');
+//TODO:
+// const currentDate = new Date();
+// console.log(currentDate);
+
+const elements = {
+    day:
+};
